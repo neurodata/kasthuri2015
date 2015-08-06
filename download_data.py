a6f0694e12419d9e74372b37ce2547cbac7aa7c9
Jordan Matelsky <j6k4m8@gmail.com>
2015-08-06 18:36:07 -0400

    Update download script

--- download_data.py
+++ download_data.py
@@ -1,5 +1,10 @@
 import ocp_access
 
+# This script downloads the Kasthuri 2014 Cell data from
+# the OCP server.
+
+# Example: Download the full dataset (TAKES A LONG TIME)
+
 ocp_access.get_data(
         token =     "kasthuri11",
         x_lo =      5000,              x_hi =      6000,
@@ -8,8 +13,13 @@ ocp_access.get_data(
         location =  "data"
 )
 
-"""
-    x_lo =      0,              x_hi =      10752,
-    y_lo =      0,              y_hi =      13312,
-    z_lo =      1,              z_hi =      1850,
-"""
+# Example: Download a small dataset
+# =================================
+#ocp_access.get_data(
+#    token =     "kasthuri11",
+#    zoom =      1,
+#    x_lo =      0,              x_hi =      10752,
+#    y_lo =      0,              y_hi =      13312,
+#    z_lo =      1,              z_hi =      1850,
+#    location =  "kasthuri-data"
+#)
