0018649d8af38f699f6d7b28c198c81c165de624
Jordan Matelsky <j6k4m8@gmail.com>
2015-08-10 11:04:38 -0400

    Fix naming conventions for new ocpaccess

--- DownloadData/download_data.py
+++ DownloadData/download_data.py
@@ -2,9 +2,9 @@ import ocpaccess.download
 
 ocpaccess.download.get_data(
         token =     "kasthuri11",
-            x_lo =      0,              x_hi =      10752,
-            y_lo =      0,              y_hi =      13312,
-            z_lo =      1,              z_hi =      1850,
+            x_start =      0,              x_stop =      10752,
+            y_start =      0,              y_stop =      13312,
+            z_start =      1,              z_stop =      1850,
         location =  "data"
 )
 
@@ -15,9 +15,9 @@ ocpaccess.download.get_data(
 """
 ocpaccess.download.get_data(
         token =     "kasthuri11",
-        x_lo =      5000,              x_hi =      6000,
-        y_lo =      5000,              y_hi =      6000,
-        z_lo =      1,                 z_hi =      185,
+        x_start =      5000,              x_stop =      6000,
+        y_start =      5000,              y_stop =      6000,
+        z_start =      1,                 z_stop =      185,
         location = "sample_data"
 )
 """
