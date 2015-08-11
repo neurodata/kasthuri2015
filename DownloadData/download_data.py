d4b1e520dee7d18e226343201814aae94fe6208f
Jordan Matelsky <j6k4m8@gmail.com>
2015-08-11 08:33:12 -0400

    Add explicit zoom ... ;)

--- DownloadData/download_data.py
+++ DownloadData/download_data.py
@@ -1,11 +1,11 @@
 import ocpaccess.download
 
 ocpaccess.download.get_data(
-        token =     "kasthuri11",
+            token =        "kasthuri11", zoom = 1,
             x_start =      0,              x_stop =      10752,
             y_start =      0,              y_stop =      13312,
             z_start =      1,              z_stop =      1850,
-        location =  "data"
+            location =     "data"
 )
 
 
@@ -14,10 +14,10 @@ ocpaccess.download.get_data(
 # the server is responding correctly.
 """
 ocpaccess.download.get_data(
-        token =     "kasthuri11",
+        token =        "kasthuri11", zoom = 1,
         x_start =      5000,              x_stop =      6000,
         y_start =      5000,              y_stop =      6000,
         z_start =      1,                 z_stop =      185,
-        location = "sample_data"
+        location =     "sample_data"
 )
 """
