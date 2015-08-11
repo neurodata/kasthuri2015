25d909d95fe4a065d91eec49f4c3e0fa810233e5
Jordan Matelsky <j6k4m8@gmail.com>
2015-08-11 11:18:40 -0400

    Add script to download by 'set'

--- DownloadData/download_data.py
+++ DownloadData/download_data.py
@@ -1,12 +1,48 @@
 import ocpaccess.download
 
-ocpaccess.download.get_data(
-            token =        "kasthuri11", zoom = 1,
-            x_start =      0,              x_stop =      10752,
-            y_start =      0,              y_stop =      13312,
-            z_start =      1,              z_stop =      1850,
-            location =     "data"
-)
+"""
+Two sets of tokens are supplied below -- `common` and `full`.
+
+common  The most common data download.)
+
+full    LARGE FILE WARNING
+        For the compressed data, visit [our website].
+"""
+
+# Common
+common = ['kasthuri11',
+          'kat11vesicles',
+          'kat11segments',
+          'kat11synapses',
+          'kat11mito']
+
+# Full
+full = ['kasthuri11',
+        'kasthuri11cc',
+        'kasthuri14Maine',
+        'kasthuri14s1colEM',
+        'kasthuri14s1colANNO',
+        'kat11mojocylinder',
+        'kat11redcylinder',
+        'kat11greencylinder',
+        'ac3',
+        'ac4',
+        'kat11vesicles',
+        'kat11segments',
+        'kat11synapses',
+        'kat11mito']
+
+download_tokens = # put `common` or `full`
+# download_tokens = common
+
+for t in download_tokens:
+    ocpaccess.download.get_data(
+        token =         t,              zoom = 1,
+        start =         0,              x_stop =      10752,
+        y_start =       0,              y_stop =      13312,
+        z_start =       1,              z_stop =      1850,
+        location =      t
+    )
 
 
 # You may want to run the code below (a much smaller sample) first,
